b3cb42c0f0b7bf94b1ee1839d26797e265e8c8b8
Dana Nazarchuk <s29888@pjwstk.edu.pl>
2025-03-03 11:28:17 +0100

    add func

--- task2/task2/Program.cs
+++ task2/task2/Program.cs
@@ -3,3 +3,9 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("My name is Bob");
 Console.WriteLine("How are you");
 Console.WriteLine("Fine");
+double GetAverage(int[] arr)
+{
+    return 0;
+}
+int[] numbers = { 1, 2, 3, 4 };
+GetAverage(numbers);
